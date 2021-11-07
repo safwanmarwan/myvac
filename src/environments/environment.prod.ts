698d31c2a94cebbc1972f7d39c5d7db4edb18aea
safwanmarwan11 <safwanmarwan11@gmail.com>
2021-11-08 00:41:18 +0800

    Install and init firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDj3aE4AbacxJBjZdyhMRm77CpErIHB_pY",
+    authDomain: "vac-record.firebaseapp.com",
+    databaseURL: "https://vac-record-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "vac-record",
+    storageBucket: "vac-record.appspot.com",
+    messagingSenderId: "201777035477",
+    appId: "1:201777035477:web:83c719b917a06832ab4a5c"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDj3aE4AbacxJBjZdyhMRm77CpErIHB_pY",
+    authDomain: "vac-record.firebaseapp.com",
+    databaseURL: "https://vac-record-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "vac-record",
+    storageBucket: "vac-record.appspot.com",
+    messagingSenderId: "201777035477",
+    appId: "1:201777035477:web:83c719b917a06832ab4a5c"
+  },
 };
 
 /*
